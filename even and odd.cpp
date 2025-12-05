4ec5ec3f7488050becb360874f3cbb78616a1c72
sam.daniel <billbrat371@gmail.com>
2025-12-05 17:37:41 +0300

    Add files via upload

---
+++ even and odd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void number (int a){
+    if(a%2 == 0){
+    cout<<"The number is even"<<endl;
+    }else{
+        cout<<"The number is odd";
+    }
+}
+int main(){
+    number(3);
+    return 0;
+}
